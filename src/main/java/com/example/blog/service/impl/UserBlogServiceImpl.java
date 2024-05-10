e653092b7d8c5cab941d858d37bc814cc9ccb82d
yohanes.cahya <yohanes^cahya@bcadigital.co.id>
2024-05-10 22:53:36 +0700

    feat: tuning test qnp

--- src/main/java/com/example/blog/service/impl/UserBlogServiceImpl.java
+++ src/main/java/com/example/blog/service/impl/UserBlogServiceImpl.java
@@ -1,16 +1,12 @@
 package com.example.blog.service.impl;
 
 import com.example.blog.dto.response.AppResponse;
-import com.example.blog.dto.response.BlogPostResponse;
-import com.example.blog.dto.response.BlogUserResponse;
 import com.example.blog.service.UserBlogService;
 import com.example.blog.webclient.JsonPlaceHolderClient;
 import java.time.LocalDateTime;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -29,19 +25,16 @@ public class UserBlogServiceImpl implements UserBlogService {
             .build()
         )
         .flatMap(appResponse -> jsonPlaceHolderClient.fetchUser()
-            .collectList()
-            .flatMap(blogUserResponses -> Flux.fromIterable(blogUserResponses)
-                .flatMap(blogUserResponse -> jsonPlaceHolderClient.fetchPostByUserId(
-                            blogUserResponse.getId()
-                        )
-                        .doOnNext(blogUserResponse::setPost)
-                )
-                .collectList()
-                .flatMap(blogPostResponses -> Mono.just(blogUserResponses))
+            .flatMap(blogUserResponse -> jsonPlaceHolderClient.fetchPostByUserId(
+                        blogUserResponse.getId()
+                    )
+                    .doOnNext(blogUserResponse::setPost)
+                    .flatMap(blogPostResponse -> Mono.just(blogUserResponse))
             )
+            .collectList()
             .doOnNext(appResponse::setResponses)
             .doOnNext(blogUserResponses -> appResponse.setEndTime(LocalDateTime.now()))
-            .thenReturn(appResponse)
-        );
+            .thenReturn(appResponse));
+
   }
 }
